fdf708c4e902dbb3576b08d2f8e7b2de2b4fa971
Iulian Matei <iulianmatei19768@gmail.com>
2023-03-24 14:27:27 +0200

    Simple measurement convertor

---
+++ convertor/convertor/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace convertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.output = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.convert = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Km",
+            "Mile",
+            "Celsius",
+            "Fahrenheit",
+            "Feet",
+            "Cm",
+            "Pounds",
+            "Kg"});
+            this.comboBox1.Location = new System.Drawing.Point(58, 165);
+            this.comboBox1.MaxDropDownItems = 4;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.TabStop = false;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Location = new System.Drawing.Point(559, 265);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(0, 16);
+            this.output.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.IntegralHeight = false;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Km",
+            "Mile",
+            "Celsius",
+            "Fahrenheit",
+            "Feet",
+            "Cm",
+            "Pounds",
+            "Kg"});
+            this.comboBox2.Location = new System.Drawing.Point(58, 257);
+            this.comboBox2.MaxDropDownItems = 4;
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(130, 24);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(58, 67);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(130, 22);
+            this.txt1.TabIndex = 3;
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(356, 257);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(75, 23);
+            this.convert.TabIndex = 6;
+            this.convert.Text = "Convert";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(644, 393);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 7;
+            this.exit.Text = "EXIT";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Valoare";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Din:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "In:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(559, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Rezulat:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Convertor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button convert;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ convertor/convertor/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace convertor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public int MaxDropDownItems { get; set; }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            comboBox1.IntegralHeight = false;
+
+            comboBox1.MaxDropDownItems = 4;
+
+        }
+
+        public double convert_Km2Mile(double m1)
+        {
+            return m1 * 0.62;
+        }
+        public double convert_Mile2KM(double m1)
+        {
+            return m1 * 1.60934;
+        }
+        public double convert_Celsius2Fah(double m1)
+        {
+            return m1 * 33.8;
+        }
+        public double convert_Fahrenheit2Celsius(double m1)
+        {
+            return m1 * -17.2222;
+        }
+        public double convert_feet2cm(double m1)
+        {
+            return m1 * 30.48;
+        }
+        public double convert_cm2feet(double m1)
+        {
+            return m1 * 0.0328084;
+        }
+        public double convert_pounds2kg(double m1)
+        {
+            return m1 * 0.453592;
+        }
+        public double convert_kg2pounds(double m1)
+        {
+            return m1 * 2.20462;
+        }
+
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            string valoare1 = txt1.Text.ToString();
+            double value1 = (double)Convert.ToDouble(valoare1);
+
+            if (comboBox1.Text == "Km" && comboBox2.Text == "Mile")
+            {
+                output.Text = Convert.ToString(convert_Km2Mile(value1));
+            }
+            else if (comboBox1.Text == "Mile" && comboBox2.Text == "Km")
+            {
+                output.Text = Convert.ToString(convert_Mile2KM(value1));
+            }
+            else if (comboBox1.Text == "Celsius" && comboBox2.Text == "Fahrenheit")
+            {
+                output.Text = Convert.ToString(convert_Celsius2Fah(value1));
+            }
+            else if (comboBox1.Text == "Fahrenheit" && comboBox2.Text == "Celsius")
+            {
+                output.Text = Convert.ToString(convert_Fahrenheit2Celsius(value1));
+            }
+            else if (comboBox1.Text == "Feet" && comboBox2.Text == "Cm")
+            {
+                output.Text = Convert.ToString(convert_feet2cm(value1));
+            }
+            else if (comboBox1.Text == "Cm" && comboBox2.Text == "Feet")
+            {
+                output.Text = Convert.ToString(convert_cm2feet(value1));
+            }
+            else if (comboBox1.Text == "Pounds" && comboBox2.Text == "Kg")
+            {
+                output.Text = Convert.ToString(convert_pounds2kg(value1));
+            }
+            else if (comboBox1.Text == "Kg" && comboBox2.Text == "Pounds")
+            {
+                output.Text = Convert.ToString(convert_kg2pounds(value1));
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid data");
+            }
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.IntegralHeight = false;
+            comboBox2.MaxDropDownItems = 4;
+        }
+    }
+}
